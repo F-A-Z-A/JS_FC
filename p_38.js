74808926b7dce07186fc0ea099de4733e9b5d521
AndreyUrazaev <f.a.z.a@bk.ru>
2023-08-27 16:14:35 +0500

    упражнение на стр.38

---
+++ p_38.js
@@ -0,0 +1,15 @@
+let codeWord_1 = 'обернись';
+let codeWord_2 = 'неужели';
+let codeWord_3 = 'лак';
+let codeWord_4 = 'огурцы';
+let codeWord_5 = 'барабан';
+let codeWord_6 = 'град';
+let codeWord_7 = 'трактор';
+let codeWord_8 = 'облако';
+let codeWord_9 = 'брат';
+let codeWord_0 = '$#%!';
+
+
+console.log(
+    codeWord_1[5] + codeWord_2[0] + codeWord_3[2] + codeWord_4[2] + codeWord_5[4] + codeWord_6[2] + codeWord_7[4] + codeWord_8[5] + codeWord_9[1] + codeWord_0[3]
+);
