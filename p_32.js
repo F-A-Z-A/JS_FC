8f2ccd19d9b555e21f5e20217d548db26d8cbef2
AndreyUrazaev <f.a.z.a@bk.ru>
2023-08-27 12:15:40 +0500

    Упражнение на стр.32

---
+++ p_32.js
@@ -0,0 +1,8 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+var secondsInAYear = secondsInAMinute * minutesInAHour * hoursInADay * daysInAYear;
+var age = 39;
+ageInSecond = age * secondsInAYear;
+console.log(ageInSecond);
