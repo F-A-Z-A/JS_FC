3a994921b4f68ae7db7762780e0796527e93eeb8
AndreyUrazaev <f.a.z.a@bk.ru>
2023-08-28 11:46:55 +0500

    exercise on page 40

---
+++ p_40.js
@@ -0,0 +1,10 @@
+
+let text = "эЙ, кАК деЛа?";
+textCapital = text[0].toUpperCase();
+textlower = text.toLowerCase().slice(1);
+
+console.log(
+    text = textCapital + textlower
+);
+
+// text[0].toUpperCase() + text.slice(1).toLowerCase();
