3819948b8b993a094520fada5a4d5483d3ae03b9
AndreyUrazaev <f.a.z.a@bk.ru>
2023-08-30 14:32:43 +0500

    exercise on page 43

---
+++ p_43.js
@@ -0,0 +1,12 @@
+let weekend = false;
+let shower = true;
+let backpack = true;
+let apple = true;
+let orange = false;
+goSchool = !weekend && shower && backpack && (apple || orange);
+
+console.log(
+    goSchool
+);
+
+//true
