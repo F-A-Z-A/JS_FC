c6832523d3cd0c42e15a280bb92f8c9791e5c95a
AndreyUrazaev <f.a.z.a@bk.ru>
2023-09-20 11:01:06 +0500

    exercise on page 101

---
+++ p_101.js
@@ -0,0 +1,13 @@
+let name = 'Andrey';
+
+if (name === "Andrey") {
+    console.log('Привет мне!')
+} else if (name === "Luda") {
+    console.log('Привет красотка!')
+} else if (name === 'Anna') {
+    console.log('Приветик Анюта!')
+} else {
+    console.log('Привет незнакомец...')
+}
+
+// Привет мне!
